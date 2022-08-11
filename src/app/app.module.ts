4559164b076e5887079dee91cf930c636775cc59
Steve Tu <stevetu11@yahoo.com>
2022-08-10 23:33:38 -0700

    Update app module with the addition of the new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,41 @@
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { httpInterceptorProviders } from './helpers/http.interceptor';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [httpInterceptorProviders],
   bootstrap: [AppComponent],
