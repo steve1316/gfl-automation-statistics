a24327e764bfda756cced007d4359d3f0b066db8
Steve Tu <stevetu11@yahoo.com>
2022-08-10 23:32:08 -0700

    Add auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,36 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const AUTH = 'auth/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(environment.API + AUTH + '/login', {
+      username,
+      password,
+    });
+  }
+
+  register(
+    username: string,
+    password: string,
+    email?: string
+  ): Observable<any> {
+    return this.http.post(environment.API + AUTH + '/register', {
+      username,
+      password,
+      email,
+    });
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(environment.API + AUTH + '/logout', {});
+  }
 }
